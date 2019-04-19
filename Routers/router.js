e9b8742593e7fd8f6a93eaf996f88215b17ea5d4
RuartRanosa <jmranosa@up.edu.ph>
2019-04-19 17:44:29 +0800

    Build_7

--- Routers/router.js
+++ Routers/router.js
@@ -7,27 +7,27 @@ app.use(cors())
 
 process.env.SECRET_KEY = 'secret'
 
-// let connection = mysql.createPool({
-//     host: '127.0.0.1',
-//     port: '3333',
-//     user: 'root',
-//     password: '',
-//     database: 'eatwise',
-//     connectionLimit: 10
-// });
-
-var mysqlHost = process.env.OPENSHIFT_MYSQL_DB_HOST || 'localhost';
-var mysqlPort = process.env.OPENSHIFT_MYSQL_DB_PORT || 3306;
-var mysqlUser = 'root'; //mysql username
-var mysqlPass = ''; //mysql password
-var mysqlDb   = 'eatwise'; //mysql database name
-
-var mysqlString = 'mysql://'   + mysqlUser + ':' + mysqlPass + '@' + mysqlHost + ':' + mysqlPort + '/' + mysqlDb;
-
-var connection = mysql.createConnection(mysqlString);
-connection.connect(function(err){
-  if (err) console.log(err);
+let connection = mysql.createPool({
+    host: '127.0.0.1',
+    // port: '3333',
+    user: 'root',
+    password: '',
+    database: 'eatwise',
+    connectionLimit: 10
 });
+
+// var mysqlHost = process.env.OPENSHIFT_MYSQL_DB_HOST || 'localhost';
+// var mysqlPort = process.env.OPENSHIFT_MYSQL_DB_PORT || 3306;
+// var mysqlUser = 'root'; //mysql username
+// var mysqlPass = ''; //mysql password
+// var mysqlDb   = 'eatwise'; //mysql database name
+
+// var mysqlString = 'mysql://'   + mysqlUser + ':' + mysqlPass + '@' + mysqlHost + ':' + mysqlPort + '/' + mysqlDb;
+
+// var connection = mysql.createConnection(mysqlString);
+// connection.connect(function(err){
+//   if (err) console.log(err);
+// });
 
 
 // instantiate controllers
@@ -44,63 +44,72 @@ app.<method>(<'/path'>, function(req, res, next){
 }, <controller_name>.<function_name>)		// call function in controller.js
 */
 
-app.get('/', (req, res, next) => {
-	res.send("Welcome to Eatwise Server!")
-	next()
-})
+connection.getConnection(function(err) {
+  if (err){
+  	throw err;
+  } 
+  else{
+  	console.log("Connected!");
+	app.get('/', (req, res, next) => {
+		res.send("Welcome to Eatwise Server!")
+		next()
+	})
 
-app.get('/view-users', (req, res, next) => {
+	app.get('/view-users', (req, res, next) => {
+			res.locals.connection = connection		
+			next()
+		}, controllerUser.viewUsers)
+
+	app.post('/add-user', (req, res, next) => {
+			res.locals.connection = connection		
+			next()
+		}, controllerUser.addUser)
+
+	app.put('/update-user', (req, res, next) => {
+			res.locals.connection = connection		
+			next()
+		}, controllerUser.updateUser)
+
+
+	app.delete('/delete-user', (req, res, next) => {
+			res.locals.connection = connection		
+			next()
+		}, controllerUser.deleteUser)
+
+
+	app.post('/register', (req, res, next) => {
+		res.locals.connection = connection	
+		next()
+	}, controllerUser.register)
+
+	app.post('/login', (req, res, next) => {
+		res.locals.connection = connection	
+		next()
+	}, controllerUser.login)
+
+	app.get('/search-all-shops', (req, res, next) =>{
 		res.locals.connection = connection		
 		next()
-	}, controllerUser.viewUsers)
+	}, controllerShop.searchByName)
 
-app.post('/add-user', (req, res, next) => {
+	app.get('/search-shop', (req, res, next) =>{
 		res.locals.connection = connection		
 		next()
-	}, controllerUser.addUser)
+	}, controllerShop.searchByNameAndId)
 
-app.put('/update-user', (req, res, next) => {
+	app.get('/view-shops', (req, res, next) =>{
 		res.locals.connection = connection		
 		next()
-	}, controllerUser.updateUser)
+	}, controllerShop.viewShops)
 
 
-app.delete('/delete-user', (req, res, next) => {
+	app.post('/add-shop', (req, res, next) => {
 		res.locals.connection = connection		
 		next()
-	}, controllerUser.deleteUser)
+	}, controllerShop.addShop)
 
-
-app.post('/register', (req, res, next) => {
-	res.locals.connection = connection	
-	next()
-}, controllerUser.register)
-
-app.post('/login', (req, res, next) => {
-	res.locals.connection = connection	
-	next()
-}, controllerUser.login)
-
-app.get('/search-all-shops', (req, res, next) =>{
-	res.locals.connection = connection		
-	next()
-}, controllerShop.searchByName)
-
-app.get('/search-shop', (req, res, next) =>{
-	res.locals.connection = connection		
-	next()
-}, controllerShop.searchByNameAndId)
-
-app.get('/view-shops', (req, res, next) =>{
-	res.locals.connection = connection		
-	next()
-}, controllerShop.viewShops)
-
-
-app.post('/add-shop', (req, res, next) => {
-	res.locals.connection = connection		
-	next()
-}, controllerShop.addShop)
+  }
+});
 
 
 module.exports = app
